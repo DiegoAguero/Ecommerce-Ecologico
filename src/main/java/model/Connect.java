5504e126306d0db80496ebbbf2142e6471f1b50e
Diego <diegotomasaguero4123@gmail.com>
2024-04-15 10:51:49 +0200

    Añado db properties y modelo connect

---
+++ src/main/java/model/Connect.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ * @author Diego Aguero, David Vazquez, David Emiliano
+ */
+public class Connect {
+    public Connect(){}
+    public static Connection getConnection() throws SQLException{
+        Connection connection = null;
+        try {
+            Properties props = new Properties();
+            props.load(new FileInputStream("config/db.properties"));
+            String URL = props.getProperty("URL");
+            String DRIVER = props.getProperty("DRIVER");
+            String USER = props.getProperty("USER");
+            String PASSWORD = props.getProperty("PASSWORD");
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | IOException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return connection;
+    }
+}
